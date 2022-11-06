c06a65a94091fb7ed01445263a228bcb0bdd634e
Vit3k05 <117566315+Vit3k05@users.noreply.github.com>
2022-11-06 10:08:36 +0100

    Update and rename .gitignore to prova.php

---
+++ prova.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Cognome: <input type="text" name="Cognome"><br>
+Name: <input type="text" name="Nome"><br>
+DataNascita: <input type="text" name="Data di nascita"><br>
+Anni di bocciatura: <input type="text" name="Anni di bocciatura"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
